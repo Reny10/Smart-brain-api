118caa910ff59981c6ca328bf9f3cf68857d21ad
Reny10 <Reny10@users.noreply.github.com>
2020-05-01 21:43:08 +0100

    'dat'

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'sladoled',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
